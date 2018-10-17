de75d980c5141b32fe6461b788c35b1863c38ee5
matthiasthermote <matthias.thermote@tvh.com>
2018-10-17 19:46:51 +0200

    Add project files.

---
+++ Les git 17_10_2018/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_git_17_10_2018
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
